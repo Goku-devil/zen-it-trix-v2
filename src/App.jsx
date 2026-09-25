673d50b7eff879780b510ad9e55ebda4c15fa3d4
Goku-devil <gokugoku0156@gmail.com>
2026-09-25 20:09:21 +0530

    Initail commit for v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+import './AppV2.css'
 import { nonTechnicalEvents, schedule, technicalEvents } from './data'
 import ContactSection from './components/ContactSection'
+import AnimatedOverlay from './components/AnimatedOverlay'
 import EventsSection from './components/EventsSection'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
@@ -15,7 +16,7 @@ function IntroStrip() {
 function App() {
     const [theme, setTheme] = useState('default')
 
-    return <main className={`theme-${theme}`}><SiteNav /><Hero /><IntroStrip /><EventsSection technicalEvents={technicalEvents} nonTechnicalEvents={nonTechnicalEvents} /><Schedule items={schedule} /><ContactSection technicalEvents={technicalEvents} nonTechnicalEvents={nonTechnicalEvents} /><Footer theme={theme} onThemeChange={setTheme} /></main>
+    return <main className={`theme-${theme}`}><AnimatedOverlay /><SiteNav /><Hero /><IntroStrip /><EventsSection technicalEvents={technicalEvents} nonTechnicalEvents={nonTechnicalEvents} /><Schedule items={schedule} /><ContactSection technicalEvents={technicalEvents} nonTechnicalEvents={nonTechnicalEvents} /><Footer theme={theme} onThemeChange={setTheme} /></main>
 }
 
 export default App
